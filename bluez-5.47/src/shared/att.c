c0ae6ccce885ebd8b56d02e6a73e7b550e225cfb
Cameron Miller <cameron.miller@homeaware.com>
2020-12-10 21:29:10 -0800

    Comment change

--- bluez-5.47/src/shared/att.c
+++ bluez-5.47/src/shared/att.c
@@ -638,6 +638,7 @@ static bool handle_error_rsp(struct bt_att *att, uint8_t *pdu,
 
 	*opcode = rsp->opcode;
 	
+	//Don't actually change security
 	return false;
 	/* Attempt to change security */
 	if (!change_security(att, rsp->ecode))
